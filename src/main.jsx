5831e0edf2d53fde7d3d11e594786e6063a4069f
yotvat <idooy11@gmail.com>
2024-03-31 19:03:38 +0300

    Apply scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './RootCmp.jsx'
-import '../src/assets/style/main.css'
+// import '../src/assets/style/main.css'
+import '../src/assets/style/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
